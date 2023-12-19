5edbec860d57157f43234bad2a494618d49945d3
RjDev <rjdevnic@gmail.com>
2023-12-19 13:14:43 +0530

    changed the public ip

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ resource "aws_security_group" "ssm_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["49.36.144.255/32"]
+    cidr_blocks = ["24.239.150.16/32"]
   }
 
   egress {
@@ -64,15 +64,15 @@ resource "aws_security_group" "ssm_sg" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["49.36.144.255/32"]
-    #cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["24.239.150.16/32"]
+    #cidr_blocks = [24.239.150.16/0"]
   }
 
   ingress {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
-    cidr_blocks = ["49.36.144.255/32"]
+    cidr_blocks = ["24.239.150.16/32"]
     #cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -81,7 +81,7 @@ resource "aws_security_group" "ssm_sg" {
     to_port   = 443
     protocol  = "tcp"
     #cidr_blocks = ["49.36.144.255/32"]
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["24.239.150.16/0"]
   }
 
 }
